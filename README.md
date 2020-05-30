ddfb78c9b46f76fbbb0fd4f11001acf99eca6bf2
Aaron <admin@datahoarder.dev>
2020-05-30 06:14:40 +0200

    Initial commit
    Signed-off-by: Aaron <admin@datahoarder.dev>

---
+++ README.md
@@ -0,0 +1,80 @@
+## About
+This script can be used to install/update a number of statically compiled applications from the command line.
+
+It makes heavy use of [gget](https://github.com/dpb587/gget) under the hood to dynamically fetch git repository assets whenever possible.
+
+## Dependencies
+
+• [gget](https://github.com/dpb587/gget) v0.2.0 or later
+
+• [tar](https://packages.debian.org/buster/tar)
+
+• [unzip](https://packages.debian.org/buster/unzip)
+
+## Installation
+
+You can install `ginstall.sh` by downloading the latest release and moving it to `/usr/local/bin`.
+
+## Usage
+
+```
+whalehub@pdh:~# ginstall.sh --help
+Usage:
+  ginstall.sh [flags] <app> <version>
+
+Examples:
+  ginstall.sh docker-compose 1.25.5
+  ginstall.sh docker-compose latest
+  ginstall.sh --check docker-compose
+  ginstall.sh --search sync
+
+Flags:
+  --check, -c           Prints the latest available version of an application
+  --help, -h            Shows this page
+  --list, -l            Prints a list of supported applications
+  --search, -s          Performs a search on the list of supported applications
+  --self-update         Updates ginstall.sh to the latest available version
+  --version, -v         Prints ginstall.sh version information
+```
+
+## Supported Applications
+
+```
+whalehub@pdh:~# ginstall.sh --list
+Application:                Repository:
+------------                -----------
+bin                         github.com/w4/bin
+composer                    github.com/composer/composer
+croc                        github.com/schollz/croc
+ctop                        github.com/bcicen/ctop
+docker-compose              github.com/docker/compose
+docker-credential-pass      github.com/docker/docker-credential-helpers
+ffmpeg                      github.com/FFmpeg/FFmpeg
+ffsend                      github.com/timvisee/ffsend
+gget                        github.com/dpb587/gget
+ginstall.sh                 github.com/whalehub/ginstall.sh
+gitea                       github.com/go-gitea/gitea
+go                          github.com/golang/go
+gosu                        github.com/tianon/gosu
+hugo                        github.com/gohugoio/hugo
+imdl                        github.com/casey/intermodal
+komga                       github.com/gotson/komga
+lazydocker                  github.com/jesseduffield/lazydocker
+lego                        github.com/go-acme/lego
+mkcert                      github.com/FiloSottile/mkcert
+nebula                      github.com/slackhq/nebula
+portainer                   github.com/portainer/portainer
+rclone                      github.com/rclone/rclone
+stdiscosrv                  github.com/syncthing/discosrv
+step                        github.com/smallstep/cli
+strelaysrv                  github.com/syncthing/relaysrv
+syncthing                   github.com/syncthing/syncthing
+tldr                        github.com/isacikgoz/tldr
+upx                         github.com/upx/upx
+vigil                       github.com/valeriansaliou/vigil
+youtube-dl                  github.com/ytdl-org/youtube-dl
+```
+
+## Development
+
+If there's a statically compiled application that you'd like to see supported by `ginstall.sh`, please feel free to either open an issue or submit a pull request.
---
+++ ginstall.sh
@@ -0,0 +1,407 @@
+#!/bin/bash
+
+GINSTALL_VERSION="1.0.0"
+
+APP_NAME="$1"
+APP_VERSION="$2"
+APP_DIR="/usr/local/bin"
+
+TMP_DIR_TAR_GZ="/tmp/ginstall.tar.gz"
+TMP_DIR_TAR_XZ="/tmp/ginstall.tar.xz"
+TMP_DIR_ZIP="/tmp/ginstall.zip"
+
+TAR_ARGS="--no-same-owner --no-same-permissions"
+
+PREFIX_V="@v"
+PREFIX_NONE="@"
+
+REPO_BIN="github.com/w4/bin"
+REPO_COMPOSER="github.com/composer/composer"
+REPO_CROC="github.com/schollz/croc"
+REPO_CTOP="github.com/bcicen/ctop"
+REPO_DOCKER_COMPOSE="github.com/docker/compose"
+REPO_DOCKER_CREDENTIAL_PASS="github.com/docker/docker-credential-helpers"
+REPO_FFMPEG="github.com/FFmpeg/FFmpeg"
+REPO_FFSEND="github.com/timvisee/ffsend"
+REPO_GGET="github.com/dpb587/gget"
+REPO_GINSTALL_SH="github.com/whalehub/ginstall.sh"
+REPO_GITEA="github.com/go-gitea/gitea"
+REPO_GO="github.com/golang/go"
+REPO_GOSU="github.com/tianon/gosu"
+REPO_HEY="github.com/rakyll/hey"
+REPO_HUGO="github.com/gohugoio/hugo"
+REPO_IMDL="github.com/casey/intermodal"
+REPO_KOMGA="github.com/gotson/komga"
+REPO_LAZYDOCKER="github.com/jesseduffield/lazydocker"
+REPO_LEGO="github.com/go-acme/lego"
+REPO_MKCERT="github.com/FiloSottile/mkcert"
+REPO_NEBULA="github.com/slackhq/nebula"
+REPO_PORTAINER="github.com/portainer/portainer"
+REPO_RCLONE="github.com/rclone/rclone"
+REPO_STDISCOSRV="github.com/syncthing/discosrv"
+REPO_STEP="github.com/smallstep/cli"
+REPO_STRELAYSRV="github.com/syncthing/relaysrv"
+REPO_SYNCTHING="github.com/syncthing/syncthing"
+REPO_TLDR="github.com/isacikgoz/tldr"
+REPO_UPX="github.com/upx/upx"
+REPO_VIGIL="github.com/valeriansaliou/vigil"
+REPO_YOUTUBE_DL="github.com/ytdl-org/youtube-dl"
+
+REPO=REPO_${2^^}
+
+SUPPORTED_APPS_HEADER="Application:                Repository:
+------------                -----------"
+
+SUPPORTED_APPS_LIST="
+bin                         $REPO_BIN
+composer                    $REPO_COMPOSER
+croc                        $REPO_CROC
+ctop                        $REPO_CTOP
+docker-compose              $REPO_DOCKER_COMPOSE
+docker-credential-pass      $REPO_DOCKER_CREDENTIAL_PASS
+ffmpeg                      $REPO_FFMPEG
+ffsend                      $REPO_FFSEND
+gget                        $REPO_GGET
+ginstall.sh                 $REPO_GINSTALL_SH
+gitea                       $REPO_GITEA
+go                          $REPO_GO
+gosu                        $REPO_GOSU
+hugo                        $REPO_HUGO
+imdl                        $REPO_IMDL
+komga                       $REPO_KOMGA
+lazydocker                  $REPO_LAZYDOCKER
+lego                        $REPO_LEGO
+mkcert                      $REPO_MKCERT
+nebula                      $REPO_NEBULA
+portainer                   $REPO_PORTAINER
+rclone                      $REPO_RCLONE
+stdiscosrv                  $REPO_STDISCOSRV
+step                        $REPO_STEP
+strelaysrv                  $REPO_STRELAYSRV
+syncthing                   $REPO_SYNCTHING
+tldr                        $REPO_TLDR
+upx                         $REPO_UPX
+vigil                       $REPO_VIGIL
+youtube-dl                  $REPO_YOUTUBE_DL"
+
+HELP="Usage:
+  ginstall.sh [flags] <app> <version>
+
+Examples:
+  ginstall.sh docker-compose 1.25.5
+  ginstall.sh docker-compose latest
+  ginstall.sh --check docker-compose
+  ginstall.sh --search sync
+
+Flags:
+  --check, -c           Prints the latest available version of an application
+  --help, -h            Shows this page
+  --list, -l            Prints a list of supported applications
+  --search, -s          Performs a search on the list of supported applications
+  --self-update         Updates ginstall.sh to the latest available version
+  --version, -v         Prints ginstall.sh version information"
+
+case "$1" in
+  "--help" | "-h")
+    echo -e "$HELP"
+    exit 0
+  ;;
+
+  "--list" | "-l")
+    echo -e "$SUPPORTED_APPS_HEADER""$SUPPORTED_APPS_LIST"
+    exit 0
+  ;;
+
+  "--search" | "-s")
+    echo -e "$SUPPORTED_APPS_HEADER""\n""$(echo -e "$SUPPORTED_APPS_LIST" | grep "$APP_VERSION")"
+    exit 0
+  ;;
+
+  "--version" | "-v")
+    echo -e "ginstall version $GINSTALL_VERSION linux/amd64"
+    exit 0
+  ;;
+esac
+
+if [ "$1" == "--check" ] || [ "$1" == "-c" ]; then
+case "$2" in
+  "bin" | "composer" | "croc" | "ctop" | "ffsend" | \
+  "gget" | "ginstall" | "gitea" | "gosu" | "hugo" | \
+  "imdl" | "komga" | "lazydocker" | "lego" | "mkcert" | \
+  "nebula" | "portainer" | "rclone" | "stdiscosrv" | \
+  "step" | "strelaysrv" | "syncthing" | "tldr" | \
+  "upx" | "vigil")
+    echo -e "The latest version of $2 is" && \
+    gget --show-ref ${!REPO}
+    exit 0
+  ;;
+
+  "docker-compose")
+    echo -e "The latest version of $2 is" && \
+    gget --show-ref ${REPO_DOCKER_COMPOSE}
+    exit 0
+  ;;
+
+  "docker-credential-pass")
+    echo -e "The latest version of $2 is" && \
+    gget --show-ref ${REPO_DOCKER_CREDENTIAL_PASS}
+    exit 0
+  ;;
+
+  "ginstall.sh")
+    echo -e "The latest version of $2 is" && \
+    gget --show-ref ${REPO_GINSTALL_SH}
+    exit 0
+  ;;
+
+  "youtube-dl")
+    echo -e "The latest version of $2 is" && \
+    gget --show-ref ${REPO_YOUTUBE_DL}
+    exit 0
+  ;;
+
+  "ffmpeg" | "hey" | "go")
+  echo -e "The $1 flag currently does not support $2."
+  exit 1
+  ;;
+esac
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e "Error: This script is not running as root or with sudo! Exiting..."
+  exit 1
+fi
+
+if [ "$1" == "--self-update" ]; then
+  gget --stdout ${REPO_GINSTALL_SH} 'ginstall.sh' > ${APP_DIR}/ginstall.sh && \
+  chmod +x ${APP_DIR}/ginstall.sh
+  exit 0
+fi
+
+if [ -z "$1" ]; then
+  echo -e "You forgot to supply an application name. Please run \"ginstall.sh --help\" for usage information."
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "You forgot to supply a version number. Please run \"ginstall.sh --help\" for usage information."
+  exit 1
+fi
+
+if [ "$2" == "latest" ]; then
+case "$1" in
+  "ffmpeg" | "go" |  "rclone" |  "stdiscosrv" |  "step" |  "strelaysrv" | "syncthing" | "upx")
+  echo -e "This application currently does not support the version argument \"$2\"."
+  exit 1
+  ;;
+
+  *)
+    APP_VERSION=""
+    PREFIX_V=""
+    PREFIX_NONE=""
+  ;;
+esac
+fi
+
+case "$1" in
+  "bin")
+    gget --stdout ${REPO_BIN}${PREFIX_V}${APP_VERSION} 'bin-v*-x86_64-unknown-linux-gnu.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "composer")
+    gget --stdout ${REPO_COMPOSER}${PREFIX_NONE}${APP_VERSION} 'composer.phar' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "croc")
+    gget --stdout ${REPO_CROC}${PREFIX_V}${APP_VERSION} 'croc_*_Linux-64bit.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "ctop")
+    gget --stdout ${REPO_CTOP}${PREFIX_V}${APP_VERSION} 'ctop-*-linux-amd64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "docker-compose")
+    gget --stdout ${REPO_DOCKER_COMPOSE}${PREFIX_NONE}${APP_VERSION} 'docker-compose-Linux-x86_64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "docker-credential-pass")
+    gget --stdout ${REPO_DOCKER_CREDENTIAL_PASS}${PREFIX_V}${APP_VERSION} 'docker-credential-pass-v*-amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "ffmpeg")
+    curl -Lf -o ${TMP_DIR_TAR_XZ} https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz && \
+    tar -xf ${TMP_DIR_TAR_XZ} -C ${APP_DIR} ffmpeg-${APP_VERSION}-amd64-static/${APP_NAME} ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_XZ} && \
+    echo -e "\n$APP_NAME v$APP_VERSION has been successfully installed."
+  ;;
+
+  "ffsend")
+    gget --stdout ${REPO_FFSEND}${PREFIX_V}${APP_VERSION} 'ffsend-v*-linux-x64-static' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "gget")
+    gget --stdout ${REPO_GGET}${PREFIX_V}${APP_VERSION} 'gget-*-linux-amd64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "ginstall.sh")
+    gget --stdout ${REPO_GINSTALL_SH}${PREFIX_V}${APP_VERSION} 'ginstall.sh' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "gitea")
+    gget --stdout ${REPO_GITEA}${PREFIX_V}${APP_VERSION} 'gitea-*-linux-amd64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "go")
+    curl -Lf -o ${TMP_DIR_TAR_GZ} https://dl.google.com/go/go${APP_VERSION}.linux-amd64.tar.gz && \
+    rm -rf /usr/local/go && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C /usr/local ${TAR_ARGS} && \
+    rm ${TMP_DIR_TAR_GZ} && \
+    echo -e "\n$APP_NAME v$APP_VERSION has been successfully installed."
+  ;;
+
+  "gosu")
+    gget --stdout ${REPO_GOSU}${PREFIX_NONE}${APP_VERSION} 'gosu-amd64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "hey")
+    curl -Lf -o ${APP_DIR}/${APP_NAME} https://storage.googleapis.com/hey-release/hey_linux_amd64 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    echo -e "\n$APP_NAME v0.1.2 has been successfully installed."
+  ;;
+
+  "hugo")
+    gget --stdout ${REPO_HUGO}${PREFIX_V}${APP_VERSION} 'hugo_extended_*_Linux-64bit.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "imdl")
+    gget --stdout ${REPO_IMDL}${PREFIX_V}${APP_VERSION} 'imdl-v*-x86_64-unknown-linux-musl.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "komga")
+    gget --stdout ${REPO_KOMGA}${PREFIX_V}${APP_VERSION} 'komga-*.jar' > ${APP_DIR}/${APP_NAME}.jar && \
+    chmod +x ${APP_DIR}/${APP_NAME}.jar
+  ;;
+
+  "lazydocker")
+    gget --stdout ${REPO_LAZYDOCKER}${PREFIX_V}${APP_VERSION} 'lazydocker_*_Linux_x86_64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "lego")
+    gget --stdout ${REPO_LEGO}${PREFIX_V}${APP_VERSION} 'lego_v*_linux_amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "mkcert")
+    gget --stdout ${REPO_MKCERT}${PREFIX_V}${APP_VERSION} 'mkcert-v*-linux-amd64' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  "nebula")
+    gget --stdout ${REPO_NEBULA}${PREFIX_V}${APP_VERSION} 'nebula-linux-amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "portainer")
+    gget --stdout ${REPO_PORTAINER}${PREFIX_NONE}${APP_VERSION} 'portainer-*-linux-amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} portainer/${APP_NAME} ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "rclone")
+    gget --stdout ${REPO_RCLONE}${PREFIX_V}${APP_VERSION} 'rclone-v*-linux-amd64.zip' > ${TMP_DIR_ZIP} && \
+    unzip -jo ${TMP_DIR_ZIP} rclone-v${APP_VERSION}-linux-amd64/rclone -d ${APP_DIR} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_ZIP}
+  ;;
+
+  "stdiscosrv")
+    gget --stdout ${REPO_STDISCOSRV}${PREFIX_V}${APP_VERSION} 'stdiscosrv-linux-amd64-v*.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} stdiscosrv-linux-amd64-v${APP_VERSION}/${APP_NAME} ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "step")
+    gget --stdout ${REPO_STEP}${PREFIX_V}${APP_VERSION} 'step_linux_*_amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} step_${APP_VERSION}/bin/${APP_NAME} ${TAR_ARGS} --strip-components=2 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "strelaysrv")
+    gget --stdout ${REPO_STRELAYSRV}${PREFIX_V}${APP_VERSION} 'strelaysrv-linux-amd64-v*.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} strelaysrv-linux-amd64-v${APP_VERSION}/${APP_NAME} ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "syncthing")
+    gget --stdout ${REPO_SYNCTHING}${PREFIX_V}${APP_VERSION} 'syncthing-linux-amd64-v*.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} syncthing-linux-amd64-v${APP_VERSION}/syncthing ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "tldr")
+    gget --stdout ${REPO_TLDR}${PREFIX_V}${APP_VERSION} 'tldr_*_linux_amd64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ${APP_NAME} ${TAR_ARGS} && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "upx")
+    gget --stdout ${REPO_UPX}${PREFIX_V}${APP_VERSION} 'upx-*-amd64_linux.tar.xz' > ${TMP_DIR_TAR_XZ} && \
+    tar -xf ${TMP_DIR_TAR_XZ} -C ${APP_DIR} upx-${APP_VERSION}-amd64_linux/${APP_NAME} ${TAR_ARGS} --strip-components=1 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_XZ}
+  ;;
+
+  "vigil")
+    gget --stdout ${REPO_VIGIL}${PREFIX_V}${APP_VERSION} 'v*-x86_64.tar.gz' > ${TMP_DIR_TAR_GZ} && \
+    tar -xf ${TMP_DIR_TAR_GZ} -C ${APP_DIR} ./vigil/${APP_NAME} ${TAR_ARGS} --strip-components=2 && \
+    chmod +x ${APP_DIR}/${APP_NAME} && \
+    rm ${TMP_DIR_TAR_GZ}
+  ;;
+
+  "youtube-dl")
+    gget --stdout ${REPO_YOUTUBE_DL}${PREFIX_NONE}${APP_VERSION} 'youtube-dl' > ${APP_DIR}/${APP_NAME} && \
+    chmod +x ${APP_DIR}/${APP_NAME}
+  ;;
+
+  *)
+    echo -e "This application is not supported by ginstall.sh. Please run \"ginstall.sh --help\" for usage information."
+    exit 1
+  ;;
+esac
